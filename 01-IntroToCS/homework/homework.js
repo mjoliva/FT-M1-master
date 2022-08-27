fe46e9df92b624a7ad1935a6b14072c204cd32a0
Jesu Oliva <olivap.mariajesus@gmail.com>
2022-08-27 11:52:08 -0400

    intro result

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,22 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+var numArray = num.split('');
+var decimal = numArray.reduce(function(acum, eLe, indice){
+  return acum + Number(eLe) * 2 ** (num.length -1 - indice)
+}, 0)
+return decimal;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  var finalResult = [];
 
+  while (num/2 > 0) {
+    finalResult = finalResult + num % 2
+    num = Math.floor(num / 2)
+  }
+  return finalResult.split('').reverse().join('');
 }
 
 
