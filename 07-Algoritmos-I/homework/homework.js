e35f5ec3fefef48fe5b5a262df1cbc2e06a38636
Jesu Oliva <olivap.mariajesus@gmail.com>
2022-09-01 08:38:45 -0400

    bubblesort

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -25,7 +25,19 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  var swap = true;
+  while(swap){
+    swap = false;
+    for(let i = 0; i < array.length; i++){
+      if(array[i] > array[i+1]){
+        let aux = array[i];
+        array[i] = array [i+1];
+        array[i+1] = aux;
+        swap = true;
+      }
+    }
+  }
+  return array;
 }
 
 
