14785c16eb27157a3864a2b5a0fa23687bd4228a
Jesu Oliva <olivap.mariajesus@gmail.com>
2022-09-04 08:11:33 -0300

    quick

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -7,6 +7,25 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  if(array.length <= 1) return array;
+  
+  var pivot = array[Math.floor(Math.random() * array.length)];
+  var leftSide = [];
+  var equals = [];
+  var rightSide = [];
+
+  for(let i = 0; i < array.length; i++){
+    if(array[i] < pivot){
+      leftSide.push(array[i])
+    } 
+    else if (array[i] === pivot){
+      equals.push(array[i])
+    }
+    else {
+      rightSide.push(array[i])
+    }
+  }
+  return quickSort(leftSide).concat(equals).concat(quickSort(rightSide));
 }
 
 function mergeSort(array) {
